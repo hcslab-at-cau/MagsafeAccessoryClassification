c939c42866083144a9b9ec343b350343eebdf6f1
Jaemin <jaeminld@naver.com>
2023-05-14 00:33:01 +0900

    euler corr

--- MatlabCode/corr_result.m
+++ MatlabCode/corr_result.m
@@ -1,26 +1,27 @@
 accId = 1;
-showTrials = 1:2;
+showTrials = 4:4;
 
-figure(11)
+figure(14)
 clf
 
 nCol = length(showTrials);
-nRow = 4;
- 
+nRow = 7;
+threshold = 0.5;
+
 for cnt = 1:length(showTrials)
-    detect = detected(accId).trial(showTrials(cnt)).filter6;
+    detect = detected(accId).trial(showTrials(cnt)).filter4;
     mag = data(accId).trial(showTrials(cnt)).mag;
     gyro = data(accId).trial(showTrials(cnt)).gyro;
     corrData = zeros(4, length(detect));
-    threshold = 0.0;
+    corrDataFilter = zeros(2, length(detect));
+    corrDataOriginal = zeros(length(detect));
 
     magEuler = zeros(length(mag.sample), 3);
-    magEuler(1, :) = [0, 0, 0];
 
     for cnt2 = 2:length(mag.sample)
         % Mag의 Rotate vector를 구하고 euler로..
-        rotMat = mag.sample(cnt2-1, :).' * pinv(mag.sample(cnt2, :)).';
-        euler = rotm2eul(rotMat, 'XYZ');
+        rotMat = mag.sample(cnt2 - 1, :).' * pinv(mag.sample(cnt2, :)).';
+        euler = rotm2eul(rotMat, 'ZYX');
         magEuler(cnt2, :) = euler;
     end
     
@@ -30,7 +31,15 @@ for cnt = 1:length(showTrials)
         for cnt3 = 1:3
             corrData(cnt3, cnt2) = corr(magEuler(curRange, cnt3), gyro.sample(curRange, cnt3));
         end
-        corrData(4, cnt2) = (corrData(1, cnt2) < threshold) & (corrData(2, cnt2)<threshold) & (corrData(3, cnt2) < threshold);
+        corrData(4, cnt2) = (corrData(1, cnt2) < threshold) & ...
+            (corrData(2, cnt2) < threshold) & (corrData(3, cnt2) < threshold);
+
+        corrDataOriginal(cnt2) = corr(mag.dAngle(curRange), gyro.dAngle(curRange)) < threshold;
+    end
+
+    for cnt2 = 1:length(detect)
+        corrDataFilter(1, cnt2) = corrData(4, cnt2) & detect(cnt2);
+        corrDataFilter(2, cnt2) = corrDataOriginal(cnt2) & detect(cnt2);
     end
 
     subplot(nRow, nCol, cnt)
@@ -49,19 +58,17 @@ for cnt = 1:length(showTrials)
 
     subplot(nRow, nCol, nCol * 3 + cnt)
     plot(corrData(4,:))
-    title('xyz Corr')
+    title('Euler Corr')
+
+    subplot(nRow, nCol, nCol * 4 + cnt)
+    plot(corrDataOriginal)
+    title('dAngle Corr')
+
+    subplot(nRow, nCol, nCol * 5 + cnt)
+    plot(corrDataFilter(1, :))
+    title('new Filtering')
+
+    subplot(nRow, nCol, nCol * 6 + cnt)
+    plot(corrDataFilter(2, :))
+    title('Original Filtering')
 end
-
-
-
-
-
-
-
-
-
-
-
-
-
-

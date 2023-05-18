627788b3ce37228d338c9a83d071162fc2e0c038
LAPTOP-J7055KG0\jaemi <jaeminld@naver.com>
2023-05-18 12:30:17 +0900

    Detection init

---
+++ MatlabCode/step2_detection_extend.m
@@ -0,0 +1,16 @@
+% Detach는 attach가 안 일어나면 일어나지 않은 event 라 생각.
+
+for cnt = 1:length(data)
+    for cnt2 = 1:nTrials
+        state = StateEnum.Init;
+        detect = detected(cnt).trial(cnt2).filter6;
+
+        for cnt3 = find(detect)'
+            
+
+
+        end
+        
+    end
+
+end

8ce17937698e923ef3e9466dadeb3b9f638eb132
LAPTOP-J7055KG0\jaemi <jaeminld@naver.com>
2023-05-22 02:50:47 +0900

    accuracy update

--- MatlabCode/step2_detection_accuracy.m
+++ MatlabCode/step2_detection_accuracy.m
@@ -3,83 +3,42 @@
 % infered magnetometer using gyroscope. 
 % It can be use to environment where has no ferromagnetic.
 
-for cnt = 1:length(data)
-    data(cnt).attachCount = 0;
-    data(cnt).detachCount = 0;
-    for cnt2 = 1:nTrials
-        detect = detected(cnt).trial(cnt2).filter7;
-        attach = zeros(1, length(detect));
-
-        f = data(cnt).name;
-        % Attach -> diff  0 -> Other value   : +1
-        % Detach -> diff  other value -> 0   : -1
-        aCnt = 0;
-        dCnt = 0;
-        attach = struct();
-        detach = struct();
-        
-        lCnt = 1;
-        for cnt3 = find(detect)'
-            beforeRange = (-150:-50) + cnt3;
-            afterRange = (50:150) + cnt3;
-
-            if beforeRange(1) < 1
-                beforeRange = beforeRange + abs(beforeRange(1)) +1;
-            end
-
-            if afterRange(end) > length(detect)
-                afterRange = afterRange - (afterRange(end)-length(detect));
-            end
-            
-            beforeData = abs(data(cnt).trial(cnt2).mag.diff(beforeRange, :));
-            beforeMean = mean(beforeData(:));
-            
-            afterData = abs(data(cnt).trial(cnt2).mag.diff(afterRange, :));
-            afterMean = mean(afterData(:));
-            
-            if afterMean > beforeMean
-                % Attach
-                aCnt = aCnt + 1;
-            else
-                % Detach
-                dCnt = dCnt + 1;
-            end
-            % lCnt = lCnt + 1;
-        end
-        data(cnt).trial(cnt2).attach = aCnt;
-        data(cnt).trial(cnt2).detach = dCnt;
-
-        
-        data(cnt).attachCount = data(cnt).attachCount + aCnt;
-        data(cnt).detachCount = data(cnt).detachCount + dCnt;
-    end
-
-    data(cnt).attachAccurracy = data(cnt).attachCount/50 * 100.0;
-    data(cnt).detachAccurracy = data(cnt).detachCount/50 * 100.0;
-end
-
-accuracy = zeros(length(data), 2);
+% 1 for attach, 2 for detach
+truePositive = zeros(2, length(data));
+falsePositive = zeros(1, length(data));
 acc = strings(1, length(data));
 
 for cnt = 1:length(data)
-    accuracy(cnt, 1) = data(cnt).attachAccurracy;
-    accuracy(cnt, 2) = data(cnt).detachAccurracy;
-    
+    truePositive(1, cnt) = detectList(cnt).tpCount(1)/50 * 100;
+    truePositive(2, cnt) = detectList(cnt).tpCount(2)/50 * 100;
+
+    falsePositive(cnt) = detectList(cnt).fpCount;
     acc(cnt) = data(cnt).name;
 end
 
-figure(20)
+figure(5)
 clf
-subplot(1, 2, 1)
-bar(accuracy(:, 1))
+
+nRows = 1;
+nCols = 3;
+
+subplot(nRows, nCols, 1);
+bar(truePositive(1, :))
 ylim([0, 100])
 grid on;
 xticklabels(acc);
 title('attach accuracy')
 
-subplot(1, 2, 2)
-bar(accuracy(:, 2))
+subplot(nRows, nCols, 2);
+bar(truePositive(2, :))
 ylim([0, 100])
 grid on;
 xticklabels(acc);
 title('detach accuracy')
+
+subplot(nRows, nCols, 3);
+bar(falsePositive)
+ylim([0, 10])
+grid on;
+xticklabels(acc);
+title('False postive')

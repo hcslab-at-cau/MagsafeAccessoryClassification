82d5334b54de231317cf912f87b3b59049256cc3
LAPTOP-J7055KG0\jaemi <jaeminld@naver.com>
2023-06-05 09:12:08 +0900

    plot data

--- MatlabCode/corr_result.m
+++ MatlabCode/corr_result.m
@@ -1,124 +1,75 @@
 accId = 1;
-showTrials = 1:1;
+trials = 6:6;
 
-figure(14)
+figure(24)
 clf
 
-nCol = length(showTrials);
+nCol = length(trials);
 nRow = 8;
-threshold = 0.5;
 
-for cnt = 1:length(showTrials)
-    detect = detected(accId).trial(showTrials(cnt)).filter7;
-    mag = data(accId).trial(showTrials(cnt)).mag;
-    gyro = data(accId).trial(showTrials(cnt)).gyro;
-    corrData = data(accId).trial(showTrials(cnt)).corr;
-    mulData = zeros(1, length(detect));
-    % corrData = zeros(4, length(detect));
-    % corrDataFilter = zeros(2, length(detect));
-    % corrDataOriginal = zeros(length(detect));
+for cnt = 1:length(trials)
+    detect7 = detected(accId).trial(trials(cnt)).filter5;
+    detect8 = detected(accId).trial(trials(cnt)).filter8;
+    mag = data(accId).trial(trials(cnt)).mag;
+    gyro = data(accId).trial(trials(cnt)).gyro;
+    corrData = data(accId).trial(trials(cnt)).corr;
 
-    % magEuler = zeros(length(mag.sample), 3);
-    % 
-    % for cnt2 = 2:length(mag.sample)
-    %     % Mag의 Rotate vector를 구하고 euler로..
-    %     rotMat = (mag.sample(cnt2 - 1, :)*1/rate).' * pinv(mag.sample(cnt2, :) * 1/rate).';
-    %     euler = rotm2eul(rotMat, 'ZYX');
-    %     magEuler(cnt2, :) = euler;
-    % end
+    range = 1:length(detect7);
+    %range = 200:900;
+    detect = detect7(range); 
+
+    % modifiedCorr = corrData(2, :);
+    % subplot(nRow, nCol, nCol + cnt)
+    % hold on
+    % plot(modifiedCorr)
+    % stem(find(detect8), modifiedCorr(detect8), 'LineStyle','none')
+    % title('modified corr')
     
-    % for cnt2 = wSize + 1:length(detect)
-    %     curRange = cnt2 - wSize + 1:cnt2;
-    % 
-    %     for cnt3 = 1:3
-    %         corrData(cnt3, cnt2) = corr(magEuler(curRange, cnt3), gyro.sample(curRange, cnt3));
-    %     end
-    %     corrData(4, cnt2) = (corrData(1, cnt2) < threshold) & ...
-    %         (corrData(2, cnt2) < threshold) & (corrData(3, cnt2) < threshold);
-    % 
-    %     corrDataOriginal(cnt2) = corr(mag.dAngle(curRange), gyro.dAngle(curRange)) < threshold;
-    % end
-    % 
-    % for cnt2 = 1:length(detect)
-    %     corrDataFilter(1, cnt2) = corrData(4, cnt2) & detect(cnt2);
-    %     corrDataFilter(2, cnt2) = corrDataOriginal(cnt2) & detect(cnt2);
-    % end
-
-    interval = 10;
-
-    % for cnt2 = interval + 1:length(detect)-10
-    %     curRange = cnt2 - 10 + 1:cnt2+10;
-    % 
-    %     corrData(cnt2) = corr(mag.dAngle(curRange), mag.inferAngle(curRange));
-    % 
-    % end
-
-    for cnt2 = 1:length(detect)
-        mulData(cnt2) = mag.dAngle(cnt2) * mag.inferAngle(cnt2);
-    end
+    diff = mag.diff(range, 2);
+    cond = mag.inferAngle > 0.05;
+    cond = cond(range);
 
     subplot(nRow, nCol, cnt)
-    plot(corrData(1, :))
-    title('corrData')
+    hold on
+    stem(find(cond), diff(cond), 'LineStyle','none')
+    stem(find(detect), diff(detect), 'LineStyle','none')
+    plot(diff)
+    legend({"infer angle > 0.05", "detect", "diff y"})
+    title('mag sample diff')
 
+    
+    magY = mag.sample(range, 2);
     subplot(nRow, nCol, nCol + cnt)
-    plot(corrData(2, :))
-    title('corrData original')
+    hold on
+    stem(find(cond), magY(cond), 'LineStyle','none')
+    plot(magY)
+    title('mag sample')
 
-    subplot(nRow, nCol, nCol * 2 + cnt)
-    plot(mulData)
-    title('mul')
+    newCorr = corrData(1, range);
+    subplot(nRow, nCol, nCol*2 + cnt)
+    hold on
+    plot(newCorr)
+    stem(find(detect), newCorr(detect), 'LineStyle','none')
+    title('new corr')
 
-    subplot(nRow, nCol, nCol* 3 + cnt)
-    plot(mag.sample)
-    title('Sample')
-
-    subplot(nRow, nCol, nCol * 4 + cnt)
-    plot(mag.inferAngle)
+    subplot(nRow, nCol, nCol*3 + cnt)
+    plot(mag.inferAngle(range))
     title('infer angle')
 
-    subplot(nRow, nCol, nCol * 5 + cnt)
-    plot(mag.dAngle)
+    subplot(nRow, nCol, nCol * 4 + cnt)
+    plot(mag.dAngle(range))
     title('mag dAngle')
 
-
-    subplot(nRow, nCol, nCol * 6 + cnt)
-    plot(gyro.dAngle)
+    subplot(nRow, nCol, nCol * 5 + cnt)
+    plot(gyro.dAngle(range))
     title('gyro dAngle')
 
+    subplot(nRow, nCol, nCol * 6 + cnt)
+    plot(detect7(range))
+    title('New Filter > 0.9')
 
     subplot(nRow, nCol, nCol * 7 + cnt)
-    plot(detect)
-    title('Filter')
+    plot(detect8(range))
+    title('Modified corr > 0.5')
     
-
-    % subplot(nRow, nCol, cnt)
-    % hold on
-    % plot(corrData(1, :))
-    % title('x Corr')
-    % 
-    % subplot(nRow, nCol, nCol + cnt)
-    % plot(corrData(2, :))
-    % title('y Corr')
-    % 
-    % subplot(nRow, nCol, nCol * 2 + cnt)
-    % plot(corrData(3,:))
-    % title('z Corr')
-    % 
-    % 
-    % subplot(nRow, nCol, nCol * 3 + cnt)
-    % plot(corrData(4,:))
-    % title('Euler Corr')
-    % 
-    % subplot(nRow, nCol, nCol * 4 + cnt)
-    % plot(corrDataOriginal)
-    % title('dAngle Corr')
-    % 
-    % subplot(nRow, nCol, nCol * 5 + cnt)
-    % plot(corrDataFilter(1, :))
-    % title('new Filtering')
-    % 
-    % subplot(nRow, nCol, nCol * 6 + cnt)
-    % plot(corrDataFilter(2, :))
-    % title('Original Filtering')
 end
--- MatlabCode/test.m
+++ MatlabCode/test.m
@@ -1,56 +1,69 @@
 accId = 1;
-trials = 1:5;
+trials = 10:10;
 
 nCol = length(trials);
-nRow = 4;
+nRow = 7;
 
-figure(nCol*nRow)
+figure(21)
 clf
 
-cnt = 1;
-for trialId = trials
+for cnt = 1:length(trials)
     objectName = data(accId).name;
-    mag = data(accId).trial(trialId).mag;
-    gyro = data(accId).trial(trialId).gyro;
-    detect = detected(accId).trial(trialId).filter6;
+    cur = data(accId).trial(trials(cnt));
+
+    mag = cur.mag;
+    magValue = mag.sample(:, 2)';
+    gyro = cur.gyro;
+    detect = detected(accId).trial(trials(cnt)).filter7;
+    diff = mag.diff;
+    corrData = cur.corr(1, :);
     
     subplot(nRow, nCol, cnt)
     hold on
     plot(mag.sample)
+    legend('x', 'y', 'z')
     if(cnt == 1)
         title(objectName)
     else
         title('magnetometer')
     end
+    
+
     subplot(nRow, nCol, nCol + cnt)
     hold on
-    plot(mag.inferMag)
-    title('Infer mag')
-    
-    diff = zeros(length(mag), 3);
-    for t = 1:length(mag.sample)-1
-        diff(t, :) = mag.sample(t, :) - mag.refInferMag100(t, :);
-    end
-
-    %subplot(nRow, nCol, nCol * 2 + cnt)
-    %hold on
-    %plot(diff)
-    %title('Infer mag using previous mag')
-    
-    subplot(nRow, nCol, nCol * 2 + cnt)
-    hold on
     plot(detect)
-    title('detected (filter5)')
-    
-    diff = zeros(length(mag), 3);
-    
-    for t = 1:length(mag.sample)-1
-        diff(t, :) = mag.sample(t, :) - mag.inferMag(t, :);
-    end
+    title('Detect Filter')
 
-    subplot(nRow, nCol, nCol * 3 + cnt)
+    subplot(nRow, nCol, nCol*2 + cnt)
     hold on
-    plot(diff)
-    title('diff')
-    cnt = cnt+1;
+    plot(corrData)
+    title('Detect Filter')
+    
+    subplot(nRow, nCol, nCol*3 + cnt)
+    hold on
+    stem(find(corrData > 0.9), magValue(corrData > 0.9), 'LineStyle','none')
+    plot(magValue)
+    legend('corr', 'y')
+    title('sample with corr > 0.9')
+
+    subplot(nRow, nCol, nCol*4 + cnt)
+    hold on
+    stem(find(detect), mag.sample(detect), 'LineStyle','none')
+    plot(mag.sample)
+    legend('filter', 'x', 'y', 'z')
+    title('sample with detect filter')
+
+    subplot(nRow, nCol, nCol*5 + cnt)
+    hold on
+    stem(find(corrData > 0.9), mag.diff(corrData > 0.9), 'LineStyle','none')
+    plot(mag.diff)
+    legend('corr', 'x', 'y', 'z')
+    title('sample with corr > 0.9 in diff')
+
+    subplot(nRow, nCol, nCol*6 + cnt)
+    hold on
+    stem(find(detect), mag.diff(detect), 'LineStyle','none')
+    plot(mag.diff)
+    legend('filter', 'x', 'y', 'z')
+    title('sample with detect filter in diff')
 end

f10050fc62ff3b34c5494ad15ed8ef10d8f06cd4
LAPTOP-J7055KG0\jaemi <jaeminld@naver.com>
2023-07-03 01:38:44 +0900

    test gyro

--- MatlabCode/step3_feature_extraction_extend.m
+++ MatlabCode/step3_feature_extraction_extend.m
@@ -3,11 +3,14 @@ rotOrder = 'XYZ';
 extractInterval = (-wSize:wSize);
 feature = struct();
 values = struct();
+values2 = struct();
 
 for cnt = 1:length(data)
     value = [];
+    value2 = [];
     feature(cnt).name = data(cnt).name;
     values(cnt).name = data(cnt).name;
+    values2(cnt).name = data(cnt).name;
     nTrials = length(data(cnt).trial);
     k = 1;
 
@@ -17,35 +20,56 @@ for cnt = 1:length(data)
         mag = data(cnt).trial(cnt2).mag;
         gyro = data(cnt).trial(cnt2).gyro;
         filterIdx = find(filter);
+        lResult = min([length(gyro.sample), length(mag.sample)]);
         
-
         for cnt3 = 1:length(filterIdx)
-            idx = filterIdx(cnt3);
-            cur(cnt3).baseRange = idx;
-            cur(cnt3).extractRange = cur(cnt3).baseRange + extractInterval;
-            
+            baseIdx = filterIdx(cnt3);
+            range = baseIdx + extractInterval;
 
-            if cur(cnt3).extractRange(1) < 1
-                cur(cnt3).extractRange = 1:cur(cnt3).extractRange(end);
+            if baseIdx < 2
+                range = 2:range(end);
             end
 
-            if cur(cnt3).extractRange(end) > length(filter)
-                cur(cnt3).extractRange = cur(cnt3).extractRange(1):length(filter);
+            if range(end) > lResult
+                range = range(1):lResult;
             end
 
-            cur(cnt3).euler = sum(gyro.sample(cur(cnt3).extractRange, :))* 1/rate;
-            cur(cnt3).rotm = eul2rotm(cur(cnt3).euler, rotOrder);
-
-            cur(cnt3).mags(1, :) = (cur(cnt3).rotm \ mag.sample(cur(cnt3).extractRange(1), :)')';
-            cur(cnt3).mags(2, :) = mag.sample(cur(cnt3).extractRange(end), :);
-            cur(cnt3).diff = cur(cnt3).mags(2, :) - cur(cnt3).mags(1, :);
-            cur(cnt3).m = sqrt(sum(cur(cnt3).diff.^2));
+            euler = sum(gyro.sample(range, :))* 1/rate;
+            rotm = eul2rotm(euler, rotOrder);
             
-            value = [value;cur(cnt3).diff];
+            refMag = mag.sample(range(1)-1, :);
+            inferredMag = zeros(1, 3);
+            for cnt4 = range
+                euler2 = gyro.sample(cnt4, :) * 1/rate;
+                rotm2 = eul2rotm(euler2, rotOrder);
+                inferredMag = (rotm2\refMag')';
+
+                refMag = inferredMag;
+            end
+
+            mags(1, :) = (rotm \ mag.sample(range(1)-1, :)')';
+            mags(2, :) = inferredMag;
+            mags(3, :) = mag.sample(range(end), :);
+            diff = mags(3, :) - mags(1, :);
+            diff2 = mags(3, :) - mags(2, :);
+            m = sqrt(sum(diff.^2));
+            
+            value = [value;diff];
+            value2 = [value2;diff2];
             k = k + 1;
+
+            cur(cnt3).startIdx = baseIdx;
+            cur(cnt3).extractRange = range;
+            cur(cnt3).euler = euler;
+            cur(cnt3).rotm = rotm;
+            cur(cnt3).mags = mags;
+            cur(cnt3).diff = diff;
+            cur(cnt3).diff2 = diff2;
+            cur(cnt3).m = m;
         end
         
         feature(cnt).trial(cnt2).cur = cur;
     end
     values(cnt).feature = value;
+    values2(cnt).feature = value2;
 end

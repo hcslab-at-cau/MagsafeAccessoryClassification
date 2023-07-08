c6977c707c063b0e07714f630513e9c1b6190555
LAPTOP-J7055KG0\jaemi <jaeminld@naver.com>
2023-07-09 02:47:04 +0900

    modify for check

--- MatlabCode/detect_diff.m
+++ MatlabCode/detect_diff.m
@@ -1,43 +1,32 @@
-accId = 12;
-showTrials =1:1;
+accId = 4;
+showTrials =4:6;
+threshold = 50;
 
 figure(9)
 clf
 
 nCol = length(showTrials);
-nRow = 5;
+nRow = 2;
+disp(data(accId).name)
 
 for cnt = 1:length(showTrials)
     detect = detected(accId).trial(showTrials(cnt)).filter6;
     mag = data(accId).trial(showTrials(cnt)).mag;
     gyro = data(accId).trial(showTrials(cnt)).gyro;
     corrData = data(accId).trial(showTrials(cnt)).corr(1, :);
+    diff = mag.diff;
+    diffMagnitude = zeros(1, length(diff(1, :)));
+
+    for cnt2 = 1:length(diffMagnitude)
+        diffMagnitude = sqrt(sum(diff.^2, 2));
+    end
+
+    diffFilter = diffMagnitude > threshold;
 
     subplot(nRow, nCol, cnt)
-    hold on
-    plot(mag.diff)
-    stem(find(detect), mag.diff(detect), 'LineStyle','none')
-    %stem(find(detect), zeros(nnz(detect), 1), 'LineStyle','none')
-    title('Diff & Detect')
+    plot(diffMagnitude)
 
     subplot(nRow, nCol, nCol + cnt)
-    plot(mag.inferMag)
-    legend('x', 'y', 'z')
-    title('Infered Mag')
-
-    subplot(nRow, nCol, nCol*2+ cnt)
-    plot(mag.sample)
-    legend('x', 'y', 'z')
-    title('Mag samples')
-
-    subplot(nRow, nCol, nCol*3 + cnt)
-    hold on
-    plot(detect)
-    %stem(range(detect), corrData(detect), 'LineStyle','none')
-    title('detect filter6')
-
-    subplot(nRow, nCol, nCol*4+ cnt)
-    plot(mag.inferAngle)
-    title('Angle between infermag ')
+    plot(diffFilter)
     
 end

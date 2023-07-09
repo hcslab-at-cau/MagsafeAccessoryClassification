b786f642d7aab53264b48bcf5ec8b90479a27746
LAPTOP-J7055KG0\jaemi <jaeminld@naver.com>
2023-07-10 02:28:11 +0900

    tmp

--- MatlabCode/detect_diff.m
+++ MatlabCode/detect_diff.m
@@ -1,6 +1,6 @@
-accId = 4;
-showTrials =4:6;
-threshold = 50;
+accId = 7;
+showTrials =8:8;
+threshold = 70;
 
 figure(9)
 clf
@@ -8,6 +8,40 @@ clf
 nCol = length(showTrials);
 nRow = 2;
 disp(data(accId).name)
+
+
+for cnt = 1:nTrials
+    tmp = data(accId).trial(cnt);
+    diff = tmp.mag.diff;
+    diffMagnitude = zeros(1, length(diff(1, :)));
+    count = 0;
+
+    for cnt2 = 1:length(diffMagnitude)
+        diffMagnitude = sqrt(sum(diff.^2, 2));
+    end
+    
+    tmpFilter = diffMagnitude > threshold;
+    groundTruthFilter = zeros(length(tmpFilter), 1);
+
+    for cnt2 = 2:length(tmpFilter)
+        if tmpFilter(cnt2-1) ~= tmpFilter(cnt2)
+            groundTruthFilter(cnt2) = 1;
+            count = count +1;
+        end
+
+    end
+    if count ~= 10
+        disp(cnt)
+    end
+
+    for cnt2 = find(groundTruthFilter)'
+        
+    end
+
+    gt = find(groundTruthFilter);
+end
+
+
 
 for cnt = 1:length(showTrials)
     detect = detected(accId).trial(showTrials(cnt)).filter6;
--- MatlabCode/step0_load_data.m
+++ MatlabCode/step0_load_data.m
@@ -1,8 +1,13 @@
 clear;
 
-path = '../Data/Default_dataset/Jaemin6';
+rawInclude = true;
+path = '../Data/Default_dataset/';
+folderName = 'Jaemin7';
+
+path = [path, folderName];
 
 c = {'Normal_objects', 'Holders'};
 postfix = char(c);
+
 
 data = load_data(path, postfix);

156ed679c7c4d05e911386e1b04b439d07dd9afe
Jaemin <jaeminld@naver.com>
2023-07-14 17:26:23 +0900

    Arrange code

--- MatlabCode/step3_feature_extraction.m
+++ MatlabCode/step3_feature_extraction.m
@@ -1,84 +1,39 @@
 rotOrder = 'XYZ';
 
-extractInterval = [-wSize / 2, wSize / 4];
+extractInterval = (-wSize:wSize);
 feature = struct();
+
 for cnt = 1:length(data)
-    feature(cnt).name = data(cnt).name;    
+    feature(cnt).name = data(cnt).name;
     nTrials = length(data(cnt).trial);
+
     for cnt2 = 1:nTrials
+        cur = struct();
+        filter = detected(cnt).trial(cnt2).filter6;    
         mag = data(cnt).trial(cnt2).mag;
         gyro = data(cnt).trial(cnt2).gyro;
+        filterIdx = find(filter);
 
-        % If detected
-        cur = struct();
-        if sum(detected(cnt).trial(cnt2).filter5) > 0
-            % Compute the range for feature extraction
-            cur.baseRange(1) = find(detected(cnt).trial(cnt2).filter5, 1);
-            cur.baseRange(2) = find(detected(cnt).trial(cnt2).filter5, 1, 'last');                      
-            cur.extractRange = cur.baseRange + extractInterval;
-           
-            % Compute the rotation matrix from "after attachment" to "before attachment"
-            cur.euler = sum(gyro.sample(cur.extractRange, :)) * 1/rate;
-            cur.rotm = eul2rotm(cur.euler, rotOrder);
-    
-            % Rotate the (before) magnetometer reading and compare the readings
-            cur.mags(1, :) = (cur.rotm \ mag.sample(cur.extractRange(1), :)')';
-            cur.mags(2, :) = mag.sample(cur.extractRange(end), :);
-            cur.diff = cur.mags(2, :) - cur.mags(1, :);
-            cur.m = sqrt(sum(cur.diff.^2));            
-        else
-            cur.baseRange = [];
-            cur.extractRange = [];
-            cur.euler = [];
-            cur.rotm = [];
-            cur.mags = [];
-            cur.diff = [0, 0, 0];
-            cur.m = 0;
+        for cnt3 = 1:length(filterIdx)
+            baseIdx = filterIdx(cnt3);
+            range = baseIdx + extractInterval;
+
+            [featureValue1, inferredMag] = func_extract_feature(mag.sample, gyro.sample, range, 1, rate);
+            [featureValue2, inferredMagTotal] = func_extract_feature(mag.sample, gyro.sample, range, length(range), rate);
+
+            mags(1, :) = inferredMag;
+            mags(2, :) = inferredMagTotal;
+            diff(1, :) = featureValue1;
+            diff(2, :) = featureValue2;
+            m = sqrt(sum(diff(1, :).^2));
+
+            cur(cnt3).baseIdx = baseIdx;
+            cur(cnt3).extractRange = range;
+            cur(cnt3).mags = mags;
+            cur(cnt3).diff = diff;
+            cur(cnt3).m = m;
         end
         
-        feature(cnt).trial(cnt2) = cur;
+        feature(cnt).trial(cnt2).cur = cur;
     end
-    
-    % Store the summary of the feature extraction result;
-    tmp = reshape([feature(cnt).trial(:).diff], 3, nTrials)';
-    for cnt2 = size(tmp, 1):-1:1
-        if tmp(cnt2, :) == [0, 0, 0]
-            tmp(cnt2, :) = [];
-        end
-    end
-
-    feature(cnt).summary = mean(tmp);
-
-    disp(data(cnt).name)
-    disp(feature(cnt).summary)
 end
-
-save([path.postfix, '.mat'], 'data', 'detected', 'feature');
-
-figure(3)
-clf
-accId = 11;
-trialId = 1;
-
-
-hold on
-mag = data(accId).trial(trialId).mag;
-gyro = data(accId).trial(trialId).gyro;
-
-cur = feature(accId).trial(trialId);
-
-subplot 211
-hold on
-plot(mag.dAngle)
-plot(gyro.dAngle)
-xline([cur.extractRange(1), cur.extractRange(end)])
-
-subplot 223
-hold on
-plot(mag.sample)
-xline([cur.extractRange(1), cur.extractRange(end)])
-
-subplot 224
-hold on
-plot(gyro.sample)
-xline([cur.extractRange(1), cur.extractRange(end)])

125ae46c2a58f3306e0a491d96fd24dc63b93de6
Jaemin <jaeminld@naver.com>
2023-07-21 17:46:41 +0900

    calculate diff using inferAngle

--- MatlabCode/plot_diff.m
+++ MatlabCode/plot_diff.m
@@ -1,28 +1,36 @@
-accId = 12;
+accId = 11;
 showTrials = 1:10;
-threshold = 70;
+threshold = 50;
 
 gt = struct();
 for cnt = 1:length(showTrials)
     tmp = data(accId).trial(showTrials(cnt));
     diff = tmp.mag.diff;
-    diffMagnitude = zeros(1, length(diff(1, :)));
-    count = 0;
+    mag = tmp.mag;
 
-    for cnt2 = 1:length(diffMagnitude)
-        diffMagnitude = sqrt(sum(diff.^2, 2));
-    end
 
-    tmpFilter = diffMagnitude > threshold;
-    groundTruthFilter = zeros(length(tmpFilter), 1);
+    groundTruthFilter = mag.inferAngle > 0.05;
 
-    for cnt2 = 2:length(tmpFilter)
-        if tmpFilter(cnt2-1) ~= tmpFilter(cnt2)
-            groundTruthFilter(cnt2) = 1;
-            count = count +1;
+    for cnt2 = find(groundTruthFilter)'
+        range = cnt2 + (0:wSize*2);
+
+        if range(end) >= length(groundTruthFilter)
+            range = range(1):length(groundTruthFilter)-1;
+        end
+            
+        if max(groundTruthFilter(1 + range)) == 1
+            if (range(1) + find(max(mag.inferAngle(range)) == mag.inferAngle(range)) -1)~=cnt2
+                groundTruthFilter(cnt2) = 0;
+            else
+                groundTruthFilter(1 + range) = 0;
+            end
+            
         end
     end
-    if count ~= 10
+
+
+    if length(find(groundTruthFilter)) ~= 10
+        disp(length(find(groundTruthFilter)))
         disp(showTrials(cnt))
     end
 
@@ -31,6 +39,8 @@ for cnt = 1:length(showTrials)
         gt(k).(['value_', num2str(showTrials(cnt))]) = cnt2;
         k = k+1;
     end
+    
+
 end
 
 
@@ -39,7 +49,8 @@ end
 figure(8)
 clf
 
-showTrials = [4, 10];
+showTrials = 1:4;
+% showTrials = 6:10;
 
 nCol = length(showTrials);
 nRow = 4;

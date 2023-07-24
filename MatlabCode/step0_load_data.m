0a16f68b31f7c76d44005bafd8c2de91f233f9a2
LAPTOP-J7055KG0\jaemi <jaeminld@naver.com>
2023-07-25 02:31:10 +0900

    add accuracy results

--- MatlabCode/step0_load_data.m
+++ MatlabCode/step0_load_data.m
@@ -3,7 +3,7 @@ clear;
 newApp = false;
 path = '../Data/';
 datasetName = 'Default_dataset';
-folderName = 'Junhyub1';
+folderName = 'jaemin3';
 
 path = [path, datasetName, '/', folderName];
 
@@ -11,9 +11,15 @@ c = {'Normal_objects', 'Holders'};
 % c = {'subway'};
 postfix = char(c);
 
-% disp(path)
-% Phyphox version
-data = func_load_data(path, postfix);
+if newApp == false
+    % Phyphox version
+    data = func_load_data(path, postfix);
+else
+    % New app version
+    data = func_load_new_data(path, postfix);
+end
 
-% New app version
-% data = func_load_new_data(path, postfix);
+run('step1_preprocessing.m')
+run('step2_detection.m')
+run('step2_detection_evaluation.m')
+% run('step3_feature_extraction_ground_truth.m')
--- MatlabCode/step2_detection_evaluation.m
+++ MatlabCode/step2_detection_evaluation.m
@@ -7,12 +7,14 @@ for cnt = 1:length(data)
     cur = struct();
     detectionGroundTruth(cnt).name = accName;
     nTrials = length(data(cnt).trial);
+    total = 0;
 
     for cnt2 = 1:nTrials
         detectTimes = rmmissing(groundTruth.([accName, '_', num2str(cnt2)]));
         attachCnt = 0;
         detachCnt = 0;    
         totalDetection = length(find(detected(cnt).trial(cnt2).filter6));
+        total = total + length(detectTimes);
         maxLength = length(detected(cnt).trial(cnt2).filter6);
         fpCount = 0;
 
@@ -40,9 +42,63 @@ for cnt = 1:length(data)
         cur.trial(cnt2).detachCount = detachCnt;
         cur.trial(cnt2).falsePositive = totalDetection - attachCnt - detachCnt + fpCount;
     end
-
+    
+    detectionGroundTruth(cnt).total = total;
     detectionGroundTruth(cnt).value = cur;
     detectionGroundTruth(cnt).attach = sum([cur.trial.attachCount], 2);
     detectionGroundTruth(cnt).detach = sum([cur.trial.detachCount], 2);
     detectionGroundTruth(cnt).fp = sum([cur.trial.falsePositive], 2);
 end
+
+%% Plot accuracy
+truePositive = zeros(2, length(detectionGroundTruth)+1);
+falsePositive = zeros(1, length(detectionGroundTruth)+1);
+accNames = {detectionGroundTruth.name};
+
+for cnt = 1:length(detectionGroundTruth)
+    detect = detectionGroundTruth(cnt);
+    truePositive(1, cnt) = detect.attach /(detect.total/2) * 100; % for attach
+    truePositive(2, cnt) = detect.detach /(detect.total/2) * 100; % for detach
+
+    falsePositive(cnt) = detect.fp;
+end
+
+truePositive(1, end) = mean(truePositive(1, 1:length(data)));
+truePositive(2, end) = mean(truePositive(2, 1:length(data)));
+falsePositive(1, end) = sum(falsePositive(1, 1:length(data)));
+accNames = [accNames, 'mean'];
+
+f = figure('Name', [folderName, '_accuracy']);
+clf
+
+f.Position(2:4) = [250, 360, 720]; 
+
+nRows = 3;
+nCols = 1;
+
+subplot(nRows, nCols, 1);
+bar(truePositive(1, :))
+ylim([0, 100])
+grid on;
+xticklabels(accNames);
+title('attach accuracy')
+
+subplot(nRows, nCols, 2);
+bar(truePositive(2, :))
+ylim([0, 100])
+grid on;
+xticklabels(accNames);
+title('detach accuracy')
+
+subplot(nRows, nCols, 3);
+bar(falsePositive)
+ylim([0, 10])
+grid on;
+xticklabels(accNames);
+title('False postive')
+
+
+
+
+
+

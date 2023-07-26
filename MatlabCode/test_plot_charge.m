5c11309424e2e3c89512e51aa45dd84b6689fd90
LAPTOP-J7055KG0\jaemi <jaeminld@naver.com>
2023-07-27 02:41:34 +0900

    plot for charging, detect, estimated detect points

---
+++ MatlabCode/test_plot_charge.m
@@ -0,0 +1,34 @@
+% Only related to charging
+chargingAcc = {charging(:).name};
+totalAcc = {data(:).name};
+
+accName = 'charger1';
+showTrials = 1:1;
+
+fig = figure(40);
+fig.Position(3:4) = [1200, 300];
+clf
+nRow = 1;
+nCol = length(showTrials);
+
+cIdx = find(ismember(chargingAcc ,accName));
+aIdx = find(ismember(totalAcc, accName));
+
+for cnt = 1:length(showTrials)
+    tmp = data(aIdx).trial(showTrials(cnt));
+    chargeTime = charging(cIdx).trial(showTrials(cnt)).charging.sample;
+    detect = tmp.detect.sample;
+    filter = detected(aIdx).trial(showTrials(cnt)).filter6;
+    mag = tmp.mag;
+
+
+    subplot(nRow, nCol, cnt)
+    hold on
+    plot(mag.diffSum)
+    stem(detect, mag.diffSum(detect), 'filled')
+    stem(find(filter), mag.diffSum(filter), 'LineStyle', '-')
+    stem(chargeTime, mag.diffSum(chargeTime), 'filled')
+    legend({'feature(diff sum)', 'Ground-truth', 'Estimated', 'charge time'})
+    
+
+end

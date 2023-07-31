893a9da9e6d90341592a5208e8a928df8e4fda3c
Jaemin <jaeminld@naver.com>
2023-07-31 17:10:17 +0900

    add Random forest

--- MatlabCode/step4_classification.m
+++ MatlabCode/step4_classification.m
@@ -7,7 +7,7 @@ if exist('featureName', 'var')
 else
     prefix.train = 'jaemin9_p2p_orient';
     % prefix.train = 'jaemin9_p2p';
-    prefix.test  = 'jaemin3_p2p';
+    prefix.test  = 'jaemin7_p2p';
     nTrainCur = 50;
 end
 
@@ -19,19 +19,20 @@ test = func_load_feature(prefix.test);
 
 % Table drop or include
 % tableInfo = {'drop', 'griptok2'};
-tableInfo = {};
 % tableInfo = [{'include'}, chargingAcc];
-[featureMatrix, totalAcc] = func_make_feature_matrix(train, test, nTrainCur, strcmp(prefix.train, prefix.test), tableInfo);
+[featureMatrix, totalAcc] = func_make_feature_matrix(train, test, nTrainCur, strcmp(prefix.train, prefix.test));
 
 
 % Template init
-template.knn = templateKNN('NumNeighbors', 21, 'Standardize',true);
+template.knn = templateKNN('NumNeighbors', 11, 'Standardize',true);
 template.svm = templateSVM('Standardize', false);
+template.randomforest = templateTree('MaxNumSplits', 9);
 
 
 % Train models
-[model.knn, predKNN, probKNN] = func_train_model(template.knn, featureMatrix, chargingAcc);
-[model.svm, predSVM, probSVM] = func_train_model(template.svm, featureMatrix, chargingAcc);
+[model.knn, predKNN, probKNN] = func_train_model(template.knn, featureMatrix, chargingAcc, false);
+[model.svm, predSVM, probSVM] = func_train_model(template.svm, featureMatrix, chargingAcc, false);
+[model.randomforest, predRandomForest, probRandomForest] = func_train_model(template.randomforest, featureMatrix, chargingAcc, false);
 
 % Accuracy of KNN
 s1 = sum(strcmp(predKNN, featureMatrix.test.label)) / length(featureMatrix.test.label)
@@ -53,6 +54,18 @@ figSVM.Position(1:4) = [500, 200, 900, 600];
 c = confusionmat(featureMatrix.test.label, predSVM, "Order", totalAcc);
 cm = confusionchart(c, totalAcc);
 cm.RowSummary = 'row-normalized';
+
+% Plot confusion matrix for random forest result
+s3 = sum(strcmp(predRandomForest, featureMatrix.test.label)) / length(featureMatrix.test.label)
+
+figRF = figure('Name','Random forest','NumberTitle','off');
+figRF.Position(1:4) = [500, 600, 900, 600];
+
+c = confusionmat(featureMatrix.test.label, predRandomForest, "Order", totalAcc);
+cm = confusionchart(c, totalAcc);
+cm.RowSummary = 'row-normalized';
+view(model.randomforest.BinaryLearners{1}.Trained{1},'Mode','graph')
+
 
 if exist('featureName', 'var')
     clearvars featureName

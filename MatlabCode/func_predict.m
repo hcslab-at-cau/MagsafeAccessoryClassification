70e70ccaa578c8097b0b4ec0c7f2641f5fc204ef
Jaemin <jaeminld@naver.com>
2023-08-01 17:44:29 +0900

    split model charging vs non-charging -ing

---
+++ MatlabCode/func_predict.m
@@ -0,0 +1,81 @@
+function [pred, prob] = func_predict(model, testData, chargingAcc, chargingState)
+if ~exist('chargingState', 'var')
+    chargingState = true;
+end
+
+[pred, scores] = predict(model, testData.data);
+
+% Get probability of each label
+prob = exp(scores) ./ sum(exp(scores),2);
+
+% Consider charging status
+if chargingState 
+    pred = func_considerCharge(testData.label, pred, prob, totalAcc, chargingAcc);
+end
+
+    function result = func_considerCharge(label, pred, prob, totalAcc, chargingAcc)
+    result = pred;
+    
+    for cnt = 1:length(prob)
+        p = prob(cnt, :);
+        
+        % Real accessory related to charging & Prediction result is related to charging
+        if ~isempty(find(ismember(chargingAcc, label(cnt)), 1)) && isempty(find(ismember(chargingAcc, result(cnt)), 1)) 
+            % disp(cnt)
+            for k = 2:length(p)
+                pLabel = totalAcc(find(p == min(maxk(p, k))));
+
+                if ~isempty(find(ismember(chargingAcc, pLabel), 1))
+                    % disp(['result has been changed 1  ', num2str(result(cnt)), ' to ', num2str(pLabel)])
+                    % disp([num2str(cnt), '_ ', num2str(pLabel)])
+                    tmp = pLabel;   
+                    if length(pLabel) ~= 1
+                        for cnt2 = 1:length(pLabel)
+                            if ~isempty(find(ismember(chargingAcc, pLabel(cnt2)), 1))
+                                tmp = pLabel(cnt2);
+                                break;
+                            end
+                        end
+                    end
+                    
+                    result(cnt) = tmp;
+                    break;
+                end
+            end
+        % Real accessory is not related to charging & Prediction result is related to charging
+        elseif isempty(find(ismember(chargingAcc, label(cnt)), 1)) && ~isempty(find(ismember(chargingAcc, result(cnt)), 1)) 
+            for k = 2:length(p)
+                pLabel = totalAcc(find(p == min(maxk(p, k))));
+    
+                if isempty(find(ismember(chargingAcc, pLabel), 1))
+                    % disp(['result has been changed 2  ', num2str(result(cnt)), ' to ', num2str(pLabel)])
+                    % disp([num2str(cnt), '_ ', num2str(pLabel)])
+
+                    tmp = pLabel;   
+                    if length(pLabel) ~= 1
+                        for cnt2 = 1:length(pLabel)
+                            if ~isempty(find(ismember(chargingAcc, pLabel(cnt2)), 1))
+                                tmp = pLabel(cnt2);
+                                break;
+                            end
+                        end
+                        if length(pLabel) ~= 1
+                            continue
+                        end
+                    end
+                    
+                    result(cnt) = tmp;
+                    % result(cnt) = pLabel;
+                    break;
+                end
+            end
+        end
+    end
+    
+    end
+
+
+
+
+end
+
--- MatlabCode/step4_classification.m
+++ MatlabCode/step4_classification.m
@@ -7,8 +7,8 @@ if exist('featureName', 'var')
     clearvars featureName
 else
     % prefix.train = 'jaemin9_p2p_orient';
-    prefix.train = 'jaemin3_p2p';
-    prefix.test  = 'jaemin8_p2p';
+    prefix.train = 'jaemin9_p2p';
+    prefix.test  = 'jaemin3_p2p';
     nTrainCur = 50;
 end
 
@@ -20,12 +20,12 @@ test = func_load_feature(prefix.test);
 
 
 % Orientation test
-addTrain = func_load_feature('orientation_p2p');
-
-for cnt = 1:length(addTrain)
-    idx = find(ismember({train.name}, addTrain(cnt).name));
-    train(idx) = addTrain(cnt);
-end
+% addTrain = func_load_feature('orientation_p2p');
+% 
+% for cnt = 1:length(addTrain)
+%     idx = find(ismember({train.name}, addTrain(cnt).name));
+%     train(idx) = addTrain(cnt);
+% end
 
 
 % Table drop or include
@@ -41,9 +41,9 @@ template.randomforest = templateTree('MaxNumSplits', 7);
 
 
 % Train models
-[model.knn, predKNN, probKNN] = func_train_model(template.knn, featureMatrix, chargingAcc, chargingInfo);
-[model.svm, predSVM, probSVM] = func_train_model(template.svm, featureMatrix, chargingAcc, chargingInfo);
-[model.randomforest, predRandomForest, probRandomForest] = func_train_model(template.randomforest, featureMatrix, chargingAcc, chargingInfo);
+[chargerModel.knn, predKNN, probKNN] = func_train_model(template.knn, featureMatrix, chargingAcc, chargingInfo);
+[chargerModel.svm, predSVM, probSVM] = func_train_model(template.svm, featureMatrix, chargingAcc, chargingInfo);
+[chargerModel.randomforest, predRandomForest, probRandomForest] = func_train_model(template.randomforest, featureMatrix, chargingAcc, chargingInfo);
 
 
 % Plot confusion matrix
@@ -85,4 +85,88 @@ cm.RowSummary = 'row-normalized';
 title('Random Forest');
 
 % view(model.randomforest.BinaryLearners{1}.Trained{1},'Mode','graph')
+return
+%% Split model to charging and non-charging
+prefix.train = 'jaemin9_p2p';
+prefix.test  = 'jaemin3_p2p';
+nTrainCur = 50;
 
+train = func_load_feature(prefix.train);
+test = func_load_feature(prefix.test);
+
+chargingAcc = {'batterypack1', 'charger1', 'charger2', 'holder2', 'holder3', 'holder4'};
+
+[~, totalAcc] = func_make_feature_matrix(train, test, nTrainCur, strcmp(prefix.train, prefix.test));  % Just extract total accessory
+otherAcc = totalAcc(~ismember(totalAcc, chargingAcc));
+
+% Split train,test dataset
+chargeTrain = train(ismember({train.name}, chargingAcc));
+chargeTest = test(ismember({test.name}, chargingAcc));
+
+otherTrain = train(ismember({train.name}, otherAcc));
+otherTest = test(ismember({test.name}, otherAcc));
+
+% Make feature matrix
+[chargeFeatureMatrix, ~] = func_make_feature_matrix(chargeTrain, chargeTest, nTrainCur, strcmp(prefix.train, prefix.test));
+[otherFeatureMatrix, ~] = func_make_feature_matrix(otherTrain, otherTest, nTrainCur, strcmp(prefix.train, prefix.test));
+
+% Template init
+template.knn = templateKNN('NumNeighbors', 11, 'Standardize',true);
+template.svm = templateSVM('Standardize', false);
+template.randomforest = templateTree('MaxNumSplits', 7);
+
+% Train models
+model(1).knn = fitcecoc(chargeFeatureMatrix.train.data, chargeFeatureMatrix.train.label, 'Learners', template.knn);
+model(1).svm = fitcecoc(chargeFeatureMatrix.train.data, chargeFeatureMatrix.train.label, 'Learners', template.svm);
+model(1).randomforest = fitcecoc(chargeFeatureMatrix.train.data, chargeFeatureMatrix.train.label, 'Learners', template.randomforest);
+
+model(2).knn = fitcecoc(otherFeatureMatrix.train.data, otherFeatureMatrix.train.label, 'Learners', template.knn);
+model(2).svm = fitcecoc(otherFeatureMatrix.train.data, otherFeatureMatrix.train.label, 'Learners', template.svm);
+model(2).randomforest = fitcecoc(otherFeatureMatrix.train.data, otherFeatureMatrix.train.label, 'Learners', template.randomforest);
+
+[pred(1).knn, prob(1).knn] = func_predict(model)
+
+
+for cnt = 1:length(model)
+    % Plot confusion matrix
+    nRow = 2;
+    nCol = 2;
+    
+    fig = figure('Name', ['train : ', prefix.train, '  test : ', prefix.test, num2str(cnt)], 'NumberTitle','off');
+    fig.Position(1:4) = [200, 0, 1600, 1600]; 
+    
+    % Accuracy of KNN
+    s1 = sum(strcmp(predKNN, featureMatrix.test.label)) / length(featureMatrix.test.label)
+    
+    % Plot confusion matrix for KNN result
+    subplot(nRow, nCol, 1); 
+    c = confusionmat(featureMatrix.test.label, predKNN, "Order", totalAcc);
+    cm = confusionchart(c, totalAcc);
+    cm.RowSummary = 'row-normalized';
+    title('KNN');
+    
+    % Accuracy of SVM
+    s2 = sum(strcmp(predSVM, featureMatrix.test.label)) / length(featureMatrix.test.label)
+    
+    % Plot confusion matrix for SVM result
+    subplot(nRow, nCol, 2); 
+    c = confusionmat(featureMatrix.test.label, predSVM, "Order", totalAcc);
+    cm = confusionchart(c, totalAcc);
+    cm.RowSummary = 'row-normalized';
+    title('SVM');
+    
+    % Accuracy of Random forest
+    s3 = sum(strcmp(predRandomForest, featureMatrix.test.label)) / length(featureMatrix.test.label)
+    
+    % Plot confusion matrix for random forest result
+    subplot(nRow, nCol, 3); 
+    c = confusionmat(featureMatrix.test.label, predRandomForest, "Order", totalAcc);
+    cm = confusionchart(c, totalAcc);
+    cm.RowSummary = 'row-normalized';
+    title('Random Forest');
+
+end
+
+
+return
+%% Orientation labeling test

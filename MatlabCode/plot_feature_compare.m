819aaf778d92c4e4b316c70175035c98bd3b2de8
Jaemin <jaeminld@naver.com>
2023-08-01 17:45:10 +0900

    compare three

--- MatlabCode/plot_feature_compare.m
+++ MatlabCode/plot_feature_compare.m
@@ -1,38 +1,10 @@
-name = 'jaemin5_p2p_wSize';
+name = 'jaemin9_p2p';
 values = func_load_feature(name);
 featureFigNum = 2;
-usingGroundTruth = true;
-
-% 
-% values = struct();
-% values2 = struct();
-% 
-% for cnt = 1:length(feature)
-%     value = [];
-%     value2 = [];
-%     values(cnt).name = feature(cnt).name;
-%     values2(cnt).name = feature(cnt).name;
-%     nTrials = length(feature(cnt).trial);
-% 
-%     for cnt2 = 1:nTrials
-%         cur = feature(cnt).trial(cnt2).cur;
-%         for cnt3 = 1:length(cur)
-%             if usingGroundTruth == true
-%                 value = [value;cur(cnt3).attach];
-%                 value2 = [value2;cur(cnt3).detach];
-%             else
-%                 value = [value;cur(cnt3).diff(1, :)];
-%                 value2 = [value2;cur(cnt3).diff(2, :)];
-%             end
-%         end 
-%     end
-%     values(cnt).feature = value;
-%     values2(cnt).feature = value2;
-% end
 
 % only plot includeTable
 includeTable = {'holder3', 'charger2'};
-0
+
 accNames = {values.name};
 
 tmpFeature = struct();
@@ -70,12 +42,10 @@ end
 xlabel('x')
 ylabel('y')
 zlabel('z')
-% xlim([-50, 0]);
-% ylim([0, 80]);
-% zlim([-50, 50]);
+
 hold on
 %% For two
-name = 'jaemin8_p2p';
+name = 'jaemin3_p2p';
 values2 = func_load_feature(name);
 
 accNames = {values2.name};
@@ -104,4 +74,38 @@ for cnt = 1:length(values2)
     end
     hold on
 end
-legend(strvcat(label, label2))
+% legend(strvcat(label, label2))
+label = strvcat(label, label2);
+
+%% For three
+name = 'orientation_p2p';
+values2 = func_load_feature(name);
+
+includeTable = {'holder3'};
+accNames = {values2.name};
+tmpFeature = struct();
+
+for cnt = 1:length(includeTable)
+    tmp = values2(contains(accNames, char(includeTable(cnt))));
+    tmpFeature(cnt).name = tmp.name;
+    tmpFeature(cnt).feature = tmp.feature;
+end 
+
+values2 = tmpFeature;
+
+label3 = [];
+for cnt = 1:length(values2)
+    label3 = strvcat(label3, [values2(cnt).name, '_', name]);
+end
+
+for cnt = 1:length(values2)
+    p = values2(cnt).feature;
+
+    if cnt > 6
+        scatter3(p(:,1), p(:,2), p(:,3), 'filled');
+    else
+        scatter3(p(:,1), p(:,2), p(:,3));
+    end
+    hold on
+end
+legend(strvcat(label, label3))

0c38b2ba5a9c322c51391e46892771e21b0b8568
Jaemin <jaeminld@naver.com>
2023-08-07 17:03:50 +0900

    test acc mag hpf

--- MatlabCode/test_hpf.m
+++ MatlabCode/test_hpf.m
@@ -1,29 +1,31 @@
+accId = 11;
+trialId = 1;
+
 rate = 100;
 order = 4;
-[b.hp, a.hp] = butter(order, 10/rate * 2, 'high');
+[b.mag, a.mag] = butter(order, 5/rate * 2, 'high');
 Wn = [5 15]/rate *2;
-[b.bp, a.bp] = butter(order, Wn, 'bandpass');
+[b.acc, a.acc] = butter(order, 30/rate * 2, 'high');
 
-accId = 2;
-trialId = 8;
 disp(data(accId).name)
 
 mag = data(accId).trial(trialId).mag.sample;
+acc=  data(accId).trial(trialId).acc.sample;
 % range = 1:length(mag);
 range = 2800:3200;
 
 
-bpMagnitude = sum(filtfilt(b.hp, a.hp, mag).^2, 2);
-hpMagnitude = sum(filtfilt(b.hp, a.hp, mag(range, :)).^2, 2);
+maghp = sum(filtfilt(b.mag, a.mag, mag).^2, 2);
+acchp = sum(filtfilt(b.acc, a.acc, acc).^2, 2);
 
 figure(24)
 clf
 
 subplot(1, 2, 1)
-plot(bpMagnitude)
-title('hp total')
+plot(acchp)
+title('acc highpass')
 
 
 subplot(1, 2, 2)
-plot(hpMagnitude)
-title('highpass')
+plot(maghp)
+title('mag highpass')

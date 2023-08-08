735106496e1f630ce9f5c16756be9ee1d9223b73
LAPTOP-J7055KG0\jaemi <jaeminld@naver.com>
2023-08-08 09:02:19 +0900

    test for rotation

--- MatlabCode/plot_feature_all.m
+++ MatlabCode/plot_feature_all.m
@@ -1,12 +1,9 @@
-
-
 valueNames = {'jaemin3_p2p', 'jaemin4_p2p', 'jaemin5_p2p', 'jaemin6_p2p', ...
 'jaemin7_p2p', 'jaemin8_p2p', 'jaemin9_p2p', 'jaemin9_p2p_orient'};
 
 names = {'jaemin3', 'jaemin4', 'jaemin5', 'jaemin6', 'jaemin7', 'jaemin8', 'jaemin9', 'jaemin9'};
 
-
-includeTable = {'wallet1'};
+includeTable = {'griptok2'};
 
 figure(1)
 clf
@@ -14,6 +11,8 @@ hold on
 
 labels = [];
 
+
+oldColor = [0.2, 0.5, 0.8];
 for cnt = 1:length(valueNames)
     % values = func_load_feature([char(names(cnt)), '_p2p']);
     values = func_load_feature(char(valueNames(cnt)));
@@ -30,13 +29,23 @@ for cnt = 1:length(valueNames)
     for cnt2 = 1:length(values)
         p = values(cnt2).feature;
         if cnt > 6
-            scatter3(p(:,1), p(:,2), p(:,3), 'filled');
+            scatter3(p(:,1), p(:,2), p(:,3), 'filled', 'MarkerFaceColor', oldColor, 'MarkerEdgeColor', oldColor);
         else
-            scatter3(p(:,1), p(:,2), p(:,3));
+            disp('hello')
+            scatter3(p(:,1), p(:,2), p(:,3), 'MarkerFaceColor', oldColor, 'MarkerEdgeColor', oldColor);
         end
         hold on
     end
 end
+randomColor = rand(1, 3);
+
+newColor = [0.5, 0.7, 0.1];
+labels = strvcat(labels, ['griptok2', '-', 'rotate']);
+
+p = features;
+scatter3(p(:,1), p(:,2), p(:,3), 'filled', 'MarkerFaceColor', newColor, 'MarkerEdgeColor', newColor);
+hold on
+
 
 legend(labels)
 xlabel('x')
--- MatlabCode/test_rotation.m
+++ MatlabCode/test_rotation.m
@@ -1,16 +1,60 @@
 accId = 1;
-trialId = 1;
+trialId = 2;
 
 tmp = data(accId).trial(trialId);
 mag = tmp.mag;
 acc = tmp.acc;
-click = tmp.detect;
+gyro = tmp.gyro;
+click = tmp.detect.sample(1);
+rate = 100;
 
-point = mag.diff(click);
+start = click-201;
 
+point = mag.sample(start, :);
+
+inferredMag = zeros(length(mag.sample), 3);
+refMag = point;
+
+for t= click-200:click+99
+    euler = gyro.sample(t, :) * 1/rate;
+    rotm = eul2rotm(euler, 'XYZ');
+
+    inferredMag(t, :) = (rotm\(refMag)')';
+    refMag = inferredMag(t, :);
+end
+
+features = [];
+
+for t = click+ 100:length(mag.diff)
+    euler = gyro.sample(t, :) * 1/rate;
+    rotm = eul2rotm(euler, 'XYZ');
+
+    inferredMag(t, :) = (rotm\(refMag)')';
+    refMag = inferredMag(t, :);
+
+    tmp = mag.sample(t, :) - refMag;
+    features = [features;tmp];
+end
 
 
 figure(123)
 clf
 
+ranges = click + 100:length(mag.diff);
+interval = fix(length(ranges)/2);
+range1 = 1:cnts;
+range2 = cnts:length(ranges);
+
+p = features(range1, :);
+scatter3(p(:,1), p(:,2), p(:,3), 'filled');
+hold on
+
+p = features(range2, :);
+scatter3(p(:,1), p(:,2), p(:,3));
+
+figure(124)
+clf
+hold on
 plot(mag.diff)
+stems = [click-200,click+100:length(mag.diff)];
+stem(stems, mag.diff(stems), 'LineStyle','none')

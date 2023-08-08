25437cc1b321cc314e82a7404089fb1c82a74d33
Jaemin <jaeminld@naver.com>
2023-08-08 15:44:01 +0900

    plot all feature with rotation

--- MatlabCode/plot_feature_all.m
+++ MatlabCode/plot_feature_all.m
@@ -1,19 +1,23 @@
 valueNames = {'jaemin3_p2p', 'jaemin4_p2p', 'jaemin5_p2p', 'jaemin6_p2p', ...
-'jaemin7_p2p', 'jaemin8_p2p', 'jaemin9_p2p', 'jaemin9_p2p_orient'};
+'jaemin7_p2p', 'jaemin8_p2p', 'jaemin9_p2p'};
 
-names = {'jaemin3', 'jaemin4', 'jaemin5', 'jaemin6', 'jaemin7', 'jaemin8', 'jaemin9', 'jaemin9'};
+names = {'jaemin3', 'jaemin4', 'jaemin5', 'jaemin6', 'jaemin7', 'jaemin8', 'jaemin9'};
+% 
+% valueNames = {'orientation_p2p'};
+% names = {'jaemin9'};
 
 includeTable = {'griptok2'};
 
-figure(1)
+figure(5)
 clf
 hold on
 
 labels = [];
 
-
-oldColor = [0.2, 0.5, 0.8];
+oldColor = rand(1, 3);
+% oldColor = [0.2, 0.5, 0.8];
 for cnt = 1:length(valueNames)
+    
     % values = func_load_feature([char(names(cnt)), '_p2p']);
     values = func_load_feature(char(valueNames(cnt)));
     accNames = {values.name};
@@ -31,21 +35,35 @@ for cnt = 1:length(valueNames)
         if cnt > 6
             scatter3(p(:,1), p(:,2), p(:,3), 'filled', 'MarkerFaceColor', oldColor, 'MarkerEdgeColor', oldColor);
         else
-            disp('hello')
             scatter3(p(:,1), p(:,2), p(:,3), 'MarkerFaceColor', oldColor, 'MarkerEdgeColor', oldColor);
         end
         hold on
     end
 end
-randomColor = rand(1, 3);
 
-newColor = [0.5, 0.7, 0.1];
-labels = strvcat(labels, ['griptok2', '-', 'rotate']);
-
+% for rotation features
 p = features;
-scatter3(p(:,1), p(:,2), p(:,3), 'filled', 'MarkerFaceColor', newColor, 'MarkerEdgeColor', newColor);
-hold on
 
+accId = 1;
+trialId = 1;
+
+tmp = data(accId).trial(trialId);
+mag = tmp.mag;
+click = tmp.detect.sample(1);
+
+ranges = click + 100:length(mag.diff);
+cnt = 4;
+interval = fix(length(ranges)/cnt);
+
+
+for cnt2 = 1:cnt
+    randomColor = rand(1, 3);
+    range = 1 + interval*(cnt2-1):interval*cnt2;
+    p = features(range, :);
+    scatter3(p(:,1), p(:,2), p(:,3), 'filled', 'MarkerFaceColor', randomColor, 'MarkerEdgeColor', randomColor);
+end
+
+labels = strvcat(labels, char({'1', '2', '3', '4'}));
 
 legend(labels)
 xlabel('x')

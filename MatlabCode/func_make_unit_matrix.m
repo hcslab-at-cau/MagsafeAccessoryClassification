f2dd4569ca3a48f56b1818f62f5ab760dc746540
Jaemin <jaeminld@naver.com>
2023-08-14 17:48:05 +0900

    add args

--- MatlabCode/func_make_unit_matrix.m
+++ MatlabCode/func_make_unit_matrix.m
@@ -1,22 +1,31 @@
-function result = func_make_unit_matrix(dataset)
+function result = func_make_unit_matrix(dataset, nTrain)
+
+if ~exist("nTrain", 'var')
+    nDataCur = length(dataset(1).feature);
+else
+    nDataCur = nTrain;
+end
+
 
 nAcc = length(dataset);
 
 lFeature = 3;
-nTrainCur = length(dataset(1).feature);
+
 
 result = [];
-result.data = zeros(nAcc * nTrainCur, lFeature);
-result.label = cell(nAcc * nTrainCur, 1);
+result.data = zeros(nAcc * nDataCur, lFeature);
+result.label = cell(nAcc * nDataCur, 1);
 
 
 for cnt = 1:nAcc
     nDatas = length(dataset(cnt).feature);
+    nIdx = false(1, nDatas);
+    nIdx(randperm(nDatas, nDataCur)) = true;
 
-    cur = dataset(cnt).feature;
+    cur = dataset(cnt).feature(nIdx, :);
     
-    range = (cnt - 1) * nDatas + (1:nDatas);
+    range = (cnt - 1) * nDataCur + (1:nDataCur);
     result.data(range, :) = [vertcat(cur)];   
-    result.label(range) = repmat({dataset(cnt).name}, nTrainCur, 1);
+    result.label(range) = repmat({dataset(cnt).name}, nDataCur, 1);
 end
 end

80120da8caa1ef1215e6208ab0362ab05636c892
LAPTOP-J7055KG0\jaemi <jaeminld@naver.com>
2023-08-16 03:08:11 +0900

    for capture detection

--- MatlabCode/plot_values.m
+++ MatlabCode/plot_values.m
@@ -1,8 +1,8 @@
 figure(8)
 clf
 
-accId = 8;
-showTrials = 1:5;
+accId = 11;
+showTrials = 1:1;
 
 nCol = length(showTrials);
 nRow = 3;
@@ -26,3 +26,40 @@ for cnt = 1:length(showTrials)
     subplot(nRow, nCol, nCol*2 + cnt)
     plot(mag.inferAngle)
 end
+
+%%
+
+tmp = data(accId).trial(3);
+mag = tmp.mag;
+gyro = tmp.gyro;
+detect = tmp.detect.sample;
+
+t = detect(1);
+ranges = t + (-200:50);
+sample = mag.sample(ranges, :);
+
+refMag = mag.sample(ranges(1), :);
+diff = zeros(length(ranges), 3);
+inferredMag = zeros(length(ranges), 3);
+inferredMag(1, :) = refMag;
+
+for cnt = 2:length(ranges)
+    euler = gyro.sample(ranges(cnt), :) * 1/100;
+    rotm = eul2rotm(euler, 'XYZ');
+
+    inferredMag(cnt, :) =(rotm\refMag')';
+    refMag= inferredMag(cnt, :);
+    diff(cnt, :) = sample(cnt, :) - inferredMag(cnt, :);
+end
+
+figure(42)
+clf
+
+subplot(3, 1, 1)
+plot(sample)
+
+subplot(3, 1, 2)
+plot(inferredMag)
+
+subplot(3, 1, 3)
+plot(diff)

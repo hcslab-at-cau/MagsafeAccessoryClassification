fe5ac6edd53de23bce6875f7a00d6a7b837a047e
Jaemin <jaeminld@naver.com>
2023-09-06 17:20:42 +0900

    need to fix overall

--- MatlabCode/step5_total_evaluation.m
+++ MatlabCode/step5_total_evaluation.m
@@ -1,3 +1,5 @@
+% fix overall
+
 if ~newApp
     groundTruth = func_load_ground_truth(datasetName, folderName);
     interval = (-100:100);
@@ -37,10 +39,25 @@ for cnt = 1:length(results)
             wRange = clickedTime + interval;
 
             if ~isempty(find(ismember(wRange, detectedTime), 1))
-                if mod(t,2) == 1 % attach
-                    attachCnt = attachCnt + 1;
-                else  % detach
+                idx = find(ismember(detectedTime, wRange));
+
+                if length(idx) ~= 1
+                    m = abs(clickedTime-idx(1));
+                    selectedIdx = idx(1);
+
+                    for k = 2:length(idx)
+                        if abs(clickedTime-idx(k)) < m
+                            m = abs(clickedTime-idx(k));
+                            selectedIdx = idx(k);
+                        end
+                    end
+                    idx = selectedIdx;
+                end
+                
+                if strcmp(result(idx).pLabel, 'detach')
                     detachCnt = detachCnt + 1;
+                else
+                    attachCnt = attachCnt + 1;
                 end
             end
         end
@@ -51,7 +68,7 @@ for cnt = 1:length(results)
     end
 
     if total == 0
-        statistics(cnt) = [];
+        statistics(cnt) = []; % fix
         continue;
     end
 
@@ -85,26 +102,44 @@ for cnt = 1:length(statistics)
         count = 0;
         
         for cnt3 = 1:length(result)
-            trueLabel = result(cnt3).label;
+            refPoint = result(cnt3).detect;
+            
+            if isempty(find((detect - 200 < refPoint) & (detect+50 > refPoint), 1))
+                trueLabel = 'undefined';  % false-positive
+            else
+                trueLabel = result(cnt3).label;
+            end
+     
             predictLabel = result(cnt3).pLabel;
 
             if strcmp(predictLabel, 'detach')
                 continue;
             end
 
-            totalAttach = totalAttach + 1;
-            
             if strcmp(trueLabel, predictLabel)
                 count = count + 1;
             end
         end
         
         cur.trial(cnt2).correct = count;
-        cur.trial(cnt2).totalAttach = totalAttach;
+        cur.trial(cnt2).totalAttach = length(detect) / 2;
+    end
+
+    
+    if isempty(fieldnames(cur))
+        cur.trial(1).correct = 0;
+        cur.trial(1).totalAttach = 0;
     end
 
     statistics(cnt).classificationResult = cur;
-    statistics(cnt).classificationAccuracy = sum([cur.trial.correct], 2)/sum([cur.trial.totalAttach], 2) * 100;
+    if isempty(statistics(cnt).attach) 
+        statistics(cnt).classificationAccuracy = 0;
+    elseif statistics(cnt).attach == 0
+        statistics(cnt).classificationAccuracy = 0;
+    else
+        statistics(cnt).classificationAccuracy = sum([cur.trial.correct], 2)/statistics(cnt).attach * 100;
+    end
+    
 end
 
 %% Show Detection accuracy
@@ -185,33 +220,50 @@ ylim([0, 100])
 grid on;
 xticklabels(accNames);
 title('classification accuracy')
-
 %% Show confusion matrix
-
 labels.predict = [];
 labels.label = [];
-totalAcc = unique(featureMatrix.label);
+totalAcc = mdl.ClassNames;
+% totalAcc{end + 1} = 'undefined';
 
 
 for cnt = 1:length(results)
     accName = results(cnt).name;
     nTrials = length(results(cnt).trial);
     
-    
     if isempty(find(ismember(totalAcc, accName), 1))
         continue;
     end
-
+            
     for cnt2 = 1:nTrials
         result = results(cnt).trial(cnt2).result;
+
+        if newApp
+            click = data(cnt).trial(cnt2).detect.sample;
+        else
+            click = rmmissing(groundTruth.([accName, '_', num2str(cnt2)]));
+        end
         
         if results(cnt).trial(cnt2).detection == 0
             continue
         end
 
+        idx = true(length(result), 1);
+
+        for cnt3 = 1:length(result)
+            k = result(cnt3).detect;
+
+            if isempty(find((click - 200 < k) & (click + 50 > k), 1)) || strcmp(result(cnt3).pLabel, 'detach') % false-positive
+                idx(cnt3) = false;
+            end
+        end
+
         preds = {result.pLabel};
-        predLabel = preds(~ismember(preds, 'detach'));
-  
+        predLabel = preds(idx);
+
+        trues = {result.label};
+        trueLabel = trues(idx);
+        
         
         labels.predict = [labels.predict, predLabel];
         labels.label = [labels.label, repmat({accName}, 1, length(predLabel))];
@@ -226,3 +278,99 @@ cm = confusionchart(c, totalAcc);
 sortClasses(cm, totalAcc)
 cm.RowSummary = 'row-normalized';
 title('confusion matrix');
+
+return;
+
+%% Evaluation knnsearch 
+distances = [];
+
+
+
+values = cell2mat({results.count});
+accNames = {distances.name};
+
+f = figure('Name', 'Distance effects');
+clf
+
+nRows = 1;
+nCols = 1;
+
+f.Position(2:4) = [250, 720, 360]; 
+
+% accuracys(end + 1) = mean(accuracys);
+% accNames{end + 1} = 'mean';
+
+subplot(nRows, nCols, 1);
+bar(values)
+ylim([0, 100])
+grid on;
+xticklabels(accNames);
+title(['Distance filtering (%) threshold : ', num2str(distanceThreshold)])
+
+return;
+%%
+if ~newApp
+    groundTruth = func_load_ground_truth(datasetName, folderName);
+    interval = (-100:100);
+else
+    interval = (-200:50);
+end
+detectAccuracy = struct();
+
+for cnt = 1:length(results)
+    cur = struct();
+    accName = data(cnt).name;
+    nTrials = length(results(cnt).trial);
+    statistics(cnt).name = accName;
+    total = 0;
+
+    for cnt2 = 1:nTrials
+        if results(cnt).trial(cnt2).detection == 0
+            continue
+        end
+        result = results(cnt).trial(cnt2).result;
+
+        if newApp
+            detect = data(cnt).trial(cnt2).detect.sample;
+        else
+            detect = rmmissing(groundTruth.([accName, '_', num2str(cnt2)]));
+        end
+        
+        attachCnt = 0;
+        detachCnt = 0; 
+        total = total + length(detect);
+
+        detectedTime = cell2mat({result.detect});
+        totalDetection = length(detectedTime);
+
+        for t = 1:length(detect)
+            clickedTime = detect(t);
+            wRange = clickedTime + interval;
+
+            if ~isempty(find(ismember(wRange, detectedTime), 1))
+                idx = detectedTime()
+                
+                if mod(t,2) == 1 % attach
+                    attachCnt = attachCnt + 1;
+                else  % detach
+                    detachCnt = detachCnt + 1;
+                end
+            end
+        end
+
+        cur.trial(cnt2).attachCount = attachCnt;
+        cur.trial(cnt2).detachCount = detachCnt;
+        cur.trial(cnt2).falsePositive = totalDetection - attachCnt - detachCnt; 
+    end
+
+    if total == 0
+        detectAccuracy(cnt) = [];
+        continue;
+    end
+
+    detectAccuracy(cnt).total = total;
+    detectAccuracy(cnt).detectionResult = cur;
+    detectAccuracy(cnt).attach = sum([cur.trial.attachCount], 2);
+    detectAccuracy(cnt).detach = sum([cur.trial.detachCount], 2);
+    detectAccuracy(cnt).fp = sum([cur.trial.falsePositive], 2);
+end

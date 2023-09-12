85783f3d586f66afb53edd2e9778714eccab6d20
Jaemin <jaeminld@naver.com>
2023-09-12 15:01:40 +0900

    modify confusion matrix

--- MatlabCode/step5_total_evaluation.m
+++ MatlabCode/step5_total_evaluation.m
@@ -5,14 +5,18 @@ else
     interval = (-200:50);
 end
 % Detection evaluation & Classifcation Accuracy filtered by detection
-
 statistics = struct();
 
+% Confusion matrix filtered by detection
+labels.predict = [];
+labels.label = [];
+totalAcc = mdl.ClassNames;
 
 for cnt = 1:length(results)
-    cur = struct();
+    tmp = struct();
     nTrials = length(results(cnt).trial);
-    statistics(cnt).name = results(cnt).name;
+    accName = results(cnt).name;
+    statistics(cnt).name = accName;
 
     for cnt2 = 1:nTrials
         result = results(cnt).trial(cnt2).result;
@@ -54,34 +58,39 @@ for cnt = 1:length(results)
                     continue;                    
                 end
 
-                cur(cnt2).trial(count).refPoint = detected(idx);
-                cur(cnt2).trial(count).clickPoint = t;
-                cur(cnt2).trial(count).pLabel = result(idx).pLabel;
+                tmp(cnt2).trial(count).refPoint = detected(idx);
+                tmp(cnt2).trial(count).clickPoint = t;
+                tmp(cnt2).trial(count).pLabel = result(idx).pLabel;
                 count = count + 1;
             end
         end
 
         % Detection accuracy
-        attachNumber = length(find(~ismember({cur(cnt2).trial.pLabel}, 'detach')));
-        cur(cnt2).attachCount = attachNumber;
-        cur(cnt2).detachCount = length(cur(cnt2).trial) - attachNumber;
-        cur(cnt2).totalDetectionCount = length(trueDetection);
-        cur(cnt2).falsePositive = length(result) - length(cur(cnt2).trial); 
+        attachNumber = length(find(~ismember({tmp(cnt2).trial.pLabel}, 'detach')));
+        tmp(cnt2).attachCount = attachNumber;
+        tmp(cnt2).detachCount = length(tmp(cnt2).trial) - attachNumber;
+        tmp(cnt2).totalDetectionCount = length(trueDetection);
+        tmp(cnt2).falsePositive = length(result) - length(tmp(cnt2).trial); 
 
         % Classification accuracy
-        predictedLabels = {cur(cnt2).trial.pLabel};
+        predictedLabels = {tmp(cnt2).trial.pLabel};
         predictedLabels(ismember(predictedLabels, 'detach')) = [];
         
-        cur(cnt2).trueLabelCount = length(find(ismember(predictedLabels, statistics(cnt).name)));
-        cur(cnt2).totalLabelCount = length(predictedLabels); 
+        tmp(cnt2).trueLabelCount = length(find(ismember(predictedLabels, statistics(cnt).name)));
+        tmp(cnt2).totalLabelCount = length(predictedLabels); 
+
+        % For confusion matrix
+        labels.predict = [labels.predict, predictedLabels];
+        labels.label = [labels.label, repmat({accName}, 1, length(predictedLabels))];
+
     end
 
-    statistics(cnt).result = cur;
-    statistics(cnt).attachAccuracy = sum(cell2mat({cur.attachCount}))/sum(cell2mat({cur.totalDetectionCount})/2) * 100;
-    statistics(cnt).detachAccuracy = sum(cell2mat({cur.detachCount}))/sum(cell2mat({cur.totalDetectionCount})/2) * 100;
-    statistics(cnt).falsePositive = sum(cell2mat({cur.falsePositive}));
+    statistics(cnt).result = tmp;
+    statistics(cnt).attachAccuracy = sum(cell2mat({tmp.attachCount}))/sum(cell2mat({tmp.totalDetectionCount})/2) * 100;
+    statistics(cnt).detachAccuracy = sum(cell2mat({tmp.detachCount}))/sum(cell2mat({tmp.totalDetectionCount})/2) * 100;
+    statistics(cnt).falsePositive = sum(cell2mat({tmp.falsePositive}));
 
-    statistics(cnt).classificationAccuracy = sum(cell2mat({cur.trueLabelCount}))/sum(cell2mat({cur.totalLabelCount})) * 100;
+    statistics(cnt).classificationAccuracy = sum(cell2mat({tmp.trueLabelCount}))/sum(cell2mat({tmp.totalLabelCount})) * 100;
 end
 
 %% Show Detection accuracy
@@ -150,58 +159,7 @@ grid on;
 xticklabels(accNames);
 title('classification accuracy')
 
-return;
 %% Show confusion matrix filtered by detection
-% fix
-
-labels.predict = [];
-labels.label = [];
-totalAcc = mdl.ClassNames;
-% totalAcc{end + 1} = 'undefined';
-
-for cnt = 1:length(results)
-    accName = results(cnt).name;
-    nTrials = length(results(cnt).trial);
-    
-    if isempty(find(ismember(totalAcc, accName), 1))
-        continue;
-    end
-            
-    for cnt2 = 1:nTrials
-        result = results(cnt).trial(cnt2).result;
-
-        if newApp
-            click = data(cnt).trial(cnt2).detect.sample;
-        else
-            click = rmmissing(groundTruth.([accName, '_', num2str(cnt2)]));
-        end
-        
-        if results(cnt).trial(cnt2).detection == 0
-            continue
-        end
-
-        idx = true(length(result), 1);
-
-        for cnt3 = 1:length(result)
-            k = result(cnt3).detect;
-
-            if isempty(find((click - 200 < k) & (click + 50 > k), 1)) || strcmp(result(cnt3).pLabel, 'detach') % false-positive
-                idx(cnt3) = false;
-            end
-        end
-
-        preds = {result.pLabel};
-        predLabel = preds(idx);
-
-        trues = {result.label};
-        trueLabel = trues(idx);
-        
-        
-        labels.predict = [labels.predict, predLabel];
-        labels.label = [labels.label, repmat({accName}, 1, length(predLabel))];
-    end
-end
-
 fig = figure('Name', 'confusion matrix', 'NumberTitle','off');
 fig.Position(1:4) = [200, 0, 800, 800]; 
 
@@ -216,6 +174,7 @@ return;
 %% Evaluation feature extraction using ground-truth
 classificationResult = struct();
 
+calibrationIntreval = (-5*wSize:-2*wSize);
 attachInterval = (-2*wSize:wSize);
 
 for cnt = 1:length(data)
@@ -223,19 +182,36 @@ for cnt = 1:length(data)
     classificationResult(cnt).name = data(cnt).name;
 
     for cnt2 = 1:length(data(cnt).trial)
+        tmp = data(cnt).trial(cnt2);
+        gyro = tmp.gyro;
+        mag = tmp.mag;
+        rmag=  tmp.rmag;
+
         if newApp
             click = data(cnt).trial(cnt2).detect.sample;
         else
             click = rmmissing(groundTruth.([accName, '_', num2str(cnt2)]));
         end
 
-        for cnt3 = click
+        for cnt3 = 1:length(click)
+            t = click(cnt3);
+            range = t + attachInterval;
+            calRange = t + calibrationIntreval;
+
+            [calm, bias, ~] = magcal(rmag.sample(calRange, :));
             
+            if mod(cnt3, 2) == 1
+                featureValue = func_extract_feature((rmag.sample(range, :)-bias)*calm, gyro.sample(range, :), 1:length(range), 4, 100);
+            else
+                featureValue = func_extract_feature_reverse((rmag.sample(range, :)-bias)*calm, gyro.sample(range, :), 1:length(range), 4, 100);
+            end
+
+            cur(cnt2).trial(cnt3).feature = featureValue;
+            cur(cnt2).trial(cnt3).pred = predict(mdl, featureValue);
+          
         end
+
     end
-
-
+    classificationResult(cnt).result= cur;
 end
 
-
-accNames
